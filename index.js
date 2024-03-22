c2b4645ebaae9dbfdc46cd19a882148ee9f13092
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-03-22 17:20:37 +0000

    implemented a fetchCoordsByIP function that returns coordinates of IP address

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 // index.js
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss')
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -10,3 +11,12 @@ const { fetchMyIP } = require('./iss');
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+fetchCoordsByIP("74.12.71.28", (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
